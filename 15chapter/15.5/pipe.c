20b494b5e2786750aa28aedbf29b2e8e96349563
maxiao1993 <soundwave_max@163.com>
2017-10-25 16:20:05 +0800

    add 15chapter

---
+++ 15chapter/15.5/pipe.c
@@ -0,0 +1,37 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXLINE 4096
+
+int
+main(void)
+{
+	int 	n;
+	int 	fd[2];
+	pid_t 	pid;
+	char	line[MAXLINE];
+
+	if (pipe(fd) < 0)
+	{
+		printf("pipe error");
+		exit(1);
+	}
+	if ((pid = fork()) < 0)
+	{
+		printf("fork error");
+		exit(1);
+	}
+	else if (pid > 0)
+	{
+		close(fd[0]);
+		write(fd[1], "hello world\n" , 12);
+	}
+	else 
+	{
+		close(fd[1]);
+		n = read(fd[0],line,MAXLINE);
+		write(STDOUT_FILENO,line,n);
+	}
+	exit(0);
+}
