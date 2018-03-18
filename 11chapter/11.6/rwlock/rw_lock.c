1699f4b3b35d52939efec15e7db230af1796c5e3
maxiao1993 <soundwave_max@163.com>
2018-03-18 21:54:55 +0800

    complete the rw_lock.c

--- 11chapter/11.6/rwlock/rw_lock.c
+++ 11chapter/11.6/rwlock/rw_lock.c
@@ -66,3 +66,54 @@ job_append(struct queue *qp, struct job *jp)
 	qp->q_tail = jp;
 	pthread_rwlock_unlock(&qp->q_lock);
 }
+
+/*
+ *	Remove the given job from a queue.
+ */
+void
+job_remove(struct queue *qp, struct job *jp)
+{
+	pthread_rwlock_wrlock(&qp->q_lock);
+	if (jp == qp->q_head)
+	{
+		qp->q_head = jp->j_next;
+		if(qp->q_tail == jp)
+			qp->q_tail = NULL;
+		else
+			jp->j_next->j_prev = jp->j_prev;
+	}
+	else if (jp == qp->q_tail)
+	{
+		qp->q_tail = jp->j_prev;
+		jp->j_prev->j_next = jp->j_next;
+	}
+	else
+	{
+		jp->j_prev->j_next = jp->j_next;
+		jp->j_next->j_prev = jp->j_prev;
+	}
+	pthread_rwlock_unlock(&qp->q_lock);
+}
+
+/*
+ *	Find a job for the given thread ID.
+ */
+struct job *
+job_find(struct queue *qp, pthread_t id)
+{
+	struct job *jp;
+
+	if (pthread_rwlock_rdlock(&qp->q_lock) != 0)
+		return(NULL);
+
+	for (jp = qp->q_head; jp != NULL; jp = jp->j_next)
+		if (pthread_equal(jp->j_id, id))
+			break;			/* just break 'for'?*/
+
+	pthread_rwlock_unlock(&qp->q_lock);
+	
+	return(jp);
+
+
+}
+
