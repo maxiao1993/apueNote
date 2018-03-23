8c10f3b15bf9df5b8ed6679e72e48cb536111843
maxiao1993 <soundwave_max@163.com>
2018-03-23 22:30:09 +0800

    modify fork

--- 8chapter/8.3/fork.c
+++ 8chapter/8.3/fork.c
@@ -1,6 +1,4 @@
-#include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
+#include"apue.h"
 
 int globvar = 6;
 char buf[] = "a write to stdout\n";
@@ -13,24 +11,18 @@ main(void)
 
 	var = 88;
 	if(write(STDOUT_FILENO,buf,sizeof(buf)-1) != sizeof(buf)-1)
-	{
-		printf("write error\n");
-		exit(1);
-	}
+		err_sys("write error");
 	printf("before fork\n");
 
 	if(pid = fork()<0)
-	{
 		printf("fork error");
-		exit(1);
-	}
 	else if (pid == 0)
 	{
 		globvar++;
 		var++;
 	}
 	else
-		sleep(8);
+		sleep(2);
 
 	printf("pid = %ld, glob = %d, var = %d\n", (long)getpid(), globvar,var);
 	exit(0);
